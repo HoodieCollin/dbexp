4af45ea6951cd540a4b44a1104ef061e079a11f7
Collin Kokotas <collinkokotas@gmail.com>
2024-04-12 13:04:26 -0700

    Add DataType enum to data_types module

--- data_types/src/lib.rs
+++ data_types/src/lib.rs
@@ -3,3 +3,28 @@ pub mod oid;
 pub mod ratio;
 pub mod timestamp;
 pub mod uid;
+
+pub enum DataType {
+    Bool,
+    U8,
+    U16,
+    U32,
+    U64,
+    U128,
+    I8,
+    I16,
+    I32,
+    I64,
+    I128,
+    R16,
+    R32,
+    R64,
+    R128,
+    Uid,
+    O16,
+    O32,
+    Decimal,
+    Timestamp,
+    String { max_length: u32 },
+    Bytes { max_length: u32 },
+}
