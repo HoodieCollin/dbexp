c4975cbaf3351d1851a2b3b93bcad279ca274df1
Collin Kokotas <collinkokotas@gmail.com>
2024-04-19 09:30:06 -0700

    Refactor Bytes and Text to utilize the new low-level types

--- data_types/src/bytes.rs
+++ data_types/src/bytes.rs
@@ -1,105 +1,59 @@
-use core::hash;
-use std::{alloc::Layout, ptr::NonNull, sync::Arc};
-
 use anyhow::Result;
-use bumpalo::Bump;
+use primitives::buffer::fixed::FixedBuffer;
+use serde::Serialize;
 
-pub struct Bytes {
-    ptr: NonNull<u8>,
-    len: u32,
-    cap: u32,
-    alloc: Arc<Bump>,
-}
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize)]
+#[repr(transparent)]
+pub struct Bytes(pub(crate) FixedBuffer<u8>);
 
 impl Bytes {
-    pub fn new(cap: u32, alloc: &Arc<Bump>) -> Self {
-        let layout = Layout::from_size_align(cap as usize, 1).expect("Invalid layout");
-        let alloc = Arc::clone(alloc);
-        let ptr = alloc.alloc_layout(layout);
-        Self {
-            ptr,
-            len: 0,
-            cap,
-            alloc,
+    pub fn new(cap: u32) -> Self {
+        Self(FixedBuffer::new(cap))
+    }
+
+    pub fn try_from_str(value: &str, cap: u32) -> Result<Self> {
+        if value.len() > cap as usize {
+            anyhow::bail!("Bytes buffer is too small for string");
         }
+
+        let mut buf = Self::new(cap);
+        buf.0.extend_from_slice(value.as_bytes())?;
+        Ok(buf)
     }
 
-    pub fn try_clone_with_capacity(&self, cap: u32, alloc: Option<&Arc<Bump>>) -> Result<Self> {
-        let mut new = Self::new(cap, alloc.unwrap_or(&self.alloc));
-        new.try_push_bytes(self.as_slice())?;
-        Ok(new)
-    }
-
-    pub unsafe fn from_parts(ptr: NonNull<u8>, len: u32, cap: u32, alloc: Arc<Bump>) -> Self {
-        Self {
-            ptr,
-            len,
-            cap,
-            alloc,
-        }
-    }
-
-    pub fn into_parts(self) -> (NonNull<u8>, u32, u32, Arc<Bump>) {
-        (self.ptr, self.len, self.cap, self.alloc)
-    }
-
-    pub fn try_from_slice(bytes: &[u8], cap: u32, alloc: &Arc<Bump>) -> Result<Self> {
+    pub fn try_from_slice(bytes: &[u8], cap: u32) -> Result<Self> {
         if bytes.len() > cap as usize {
             anyhow::bail!("Bytes buffer is too small for slice");
         }
 
-        let mut buf = Self::new(cap, alloc);
-        buf.try_push_bytes(bytes)?;
+        let mut buf = Self::new(cap);
+        buf.0.extend_from_slice(bytes)?;
         Ok(buf)
     }
 
-    pub fn try_from_i128(value: i128, cap: u32, alloc: &Arc<Bump>) -> Result<Self> {
+    pub fn try_from_i128(value: i128, cap: u32) -> Result<Self> {
         if cap < 16 {
             return Err(anyhow::anyhow!("Buffer is too small for i128"));
         }
 
-        let mut buf = Self::new(cap, alloc);
-        buf.try_push_bytes(&value.to_ne_bytes())?;
+        let mut buf = Self::new(cap);
+        buf.0.extend_from_slice(&value.to_ne_bytes())?;
         Ok(buf)
     }
 
-    pub fn try_from_f64(value: f64, cap: u32, alloc: &Arc<Bump>) -> Result<Self> {
+    pub fn try_from_f64(value: f64, cap: u32) -> Result<Self> {
         if cap < 8 {
             return Err(anyhow::anyhow!("Buffer is too small for f64"));
         }
 
-        let mut buf = Self::new(cap, alloc);
-        buf.try_push_bytes(&value.to_ne_bytes())?;
+        let mut buf = Self::new(cap);
+        buf.0.extend_from_slice(&value.to_ne_bytes())?;
         Ok(buf)
     }
 
+    #[allow(dead_code)]
     #[inline(always)]
-    pub fn as_ptr(&self) -> *mut u8 {
-        self.ptr.as_ptr()
-    }
-
-    #[inline(always)]
-    pub fn as_slice(&self) -> &[u8] {
-        unsafe { std::slice::from_raw_parts(self.ptr.as_ptr(), self.len as usize) }
-    }
-
-    #[inline(always)]
-    pub fn as_slice_mut(&mut self) -> &mut [u8] {
-        unsafe { std::slice::from_raw_parts_mut(self.ptr.as_ptr(), self.len as usize) }
-    }
-
-    #[inline(always)]
-    pub fn len(&self) -> usize {
-        self.len as usize
-    }
-
-    #[inline(always)]
-    pub fn capacity(&self) -> usize {
-        self.cap as usize
-    }
-
-    #[inline(always)]
-    fn available(&self) -> usize {
+    fn available(&self) -> u32 {
         self.capacity() - self.len()
     }
 
@@ -115,100 +69,48 @@ impl Bytes {
 
     #[inline(always)]
     pub fn clear(&mut self) {
-        self.len = 0;
+        self.0.clear();
     }
 
-    pub fn alloc(&self) -> &Arc<Bump> {
-        &self.alloc
-    }
-
-    pub fn try_push_bytes(&mut self, bytes: &[u8]) -> Result<()> {
-        let val_len = bytes.len();
-
-        if val_len > self.available() {
-            return Err(anyhow::anyhow!("Bytes buffer is full"));
-        }
-
-        unsafe {
-            let dst = self.ptr.as_ptr().add(self.len as usize);
-            std::ptr::copy_nonoverlapping(bytes.as_ptr(), dst, val_len);
-            self.len += val_len as u32;
-        }
-
-        Ok(())
+    pub fn try_push_bytes(&mut self, bytes: impl AsRef<[u8]>) -> Result<()> {
+        self.0.extend_from_slice(bytes.as_ref())
     }
 }
 
 impl std::ops::Deref for Bytes {
-    type Target = [u8];
+    type Target = FixedBuffer<u8>;
 
     fn deref(&self) -> &Self::Target {
-        self.as_slice()
+        &self.0
     }
 }
 
 impl std::ops::DerefMut for Bytes {
     fn deref_mut(&mut self) -> &mut Self::Target {
-        self.as_slice_mut()
+        &mut self.0
     }
 }
 
 impl AsRef<[u8]> for Bytes {
     fn as_ref(&self) -> &[u8] {
-        self.as_slice()
+        self.0.as_slice()
     }
 }
 
 impl AsMut<[u8]> for Bytes {
     fn as_mut(&mut self) -> &mut [u8] {
-        self.as_slice_mut()
+        self.0.as_slice_mut()
     }
 }
 
-impl std::fmt::Debug for Bytes {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:?}", self.as_slice())
+impl AsRef<FixedBuffer<u8>> for Bytes {
+    fn as_ref(&self) -> &FixedBuffer<u8> {
+        &self.0
     }
 }
 
-impl serde::Serialize for Bytes {
-    fn serialize<S: serde::Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
-        serializer.serialize_bytes(self.as_slice())
-    }
-}
-
-impl Clone for Bytes {
-    fn clone(&self) -> Self {
-        let mut new = Self::new(self.cap, &self.alloc);
-        new.try_push_bytes(self.as_slice())
-            .expect("Failed to clone Bytes");
-
-        new
-    }
-}
-
-impl PartialEq for Bytes {
-    fn eq(&self, other: &Self) -> bool {
-        self.as_slice() == other.as_slice()
-    }
-}
-
-impl Eq for Bytes {}
-
-impl PartialOrd for Bytes {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        self.len().partial_cmp(&other.len())
-    }
-}
-
-impl Ord for Bytes {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        self.len().cmp(&other.len())
-    }
-}
-
-impl hash::Hash for Bytes {
-    fn hash<H: hash::Hasher>(&self, state: &mut H) {
-        self.as_slice().hash(state);
+impl AsMut<FixedBuffer<u8>> for Bytes {
+    fn as_mut(&mut self) -> &mut FixedBuffer<u8> {
+        &mut self.0
     }
 }
--- data_types/src/text.rs
+++ data_types/src/text.rs
@@ -1,67 +1,24 @@
-use std::{ptr::NonNull, sync::Arc};
-
 use super::bytes::Bytes;
 use anyhow::Result;
-use bumpalo::Bump;
 
 #[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 #[repr(transparent)]
 pub struct Text(Bytes);
 
 impl Text {
-    pub fn new(cap: u32, alloc: &Arc<Bump>) -> Self {
-        Self(Bytes::new(cap, alloc))
+    pub fn new(cap: u32) -> Self {
+        Self(Bytes::new(cap))
     }
 
-    pub fn try_clone_with_capacity(&self, cap: u32, alloc: Option<&Arc<Bump>>) -> Result<Self> {
-        let mut new = Self(Bytes::new(cap, alloc.unwrap_or(self.alloc())));
-        new.try_push_str(self.as_str())?;
-        Ok(new)
-    }
-
-    pub unsafe fn from_parts(ptr: NonNull<u8>, len: u32, cap: u32, alloc: Arc<Bump>) -> Self {
-        Self(Bytes::from_parts(ptr, len, cap, alloc))
-    }
-
-    pub fn into_parts(self) -> (NonNull<u8>, u32, u32, Arc<Bump>) {
-        self.0.into_parts()
-    }
-
-    pub fn try_from_str(value: &str, cap: u32, alloc: &Arc<Bump>) -> Result<Self> {
+    pub fn try_from_str(value: &str, cap: u32) -> Result<Self> {
         if value.len() > cap as usize {
             anyhow::bail!("Text buffer is too small for string");
         }
 
-        Ok(Self(Bytes::try_from_slice(value.as_bytes(), cap, alloc)?))
+        Ok(Self(Bytes::try_from_slice(value.as_bytes(), cap)?))
     }
 
-    pub fn try_from_i128(value: i128, cap: u32, alloc: &Arc<Bump>) -> Result<Self> {
-        let mut num = itoa::Buffer::new();
-        let value = num.format(value);
-
-        if value.len() > cap as usize {
-            anyhow::bail!("Text buffer is too small for this i128");
-        }
-
-        let mut buf = Self::new(cap, alloc);
-        buf.try_push_str(value)?;
-        Ok(buf)
-    }
-
-    pub fn try_from_f64(value: f64, cap: u32, alloc: &Arc<Bump>) -> Result<Self> {
-        let mut num = ryu::Buffer::new();
-        let value = num.format(value);
-
-        if value.len() > cap as usize {
-            anyhow::bail!("Text buffer is too small for this f64");
-        }
-
-        let mut buf = Self::new(cap, alloc);
-        buf.try_push_str(value)?;
-        Ok(buf)
-    }
-
-    pub fn try_from_slice(bytes: &[u8], cap: u32, alloc: &Arc<Bump>) -> Result<Self> {
+    pub fn try_from_slice(bytes: &[u8], cap: u32) -> Result<Self> {
         if bytes.len() > cap as usize {
             anyhow::bail!("Text buffer is too small for slice");
         }
@@ -69,62 +26,49 @@ impl Text {
         // SAFETY: bytes is guaranteed to be valid UTF-8
         std::str::from_utf8(bytes)?;
 
-        Ok(Self(Bytes::try_from_slice(bytes, cap, alloc)?))
+        Ok(Self(Bytes::try_from_slice(bytes, cap)?))
     }
 
-    pub fn as_ptr(&self) -> *mut u8 {
-        self.0.as_ptr()
-    }
+    pub fn try_from_i128(value: i128, cap: u32) -> Result<Self> {
+        let mut num = itoa::Buffer::new();
+        let value = num.format(value);
 
-    #[inline(always)]
-    pub fn as_str(&self) -> &str {
-        unsafe {
-            std::str::from_utf8_unchecked(std::slice::from_raw_parts(
-                self.as_ptr() as _,
-                self.len(),
-            ))
+        if value.len() > cap as usize {
+            anyhow::bail!("Text buffer is too small for this i128");
         }
+
+        let mut buf = Self::new(cap);
+        buf.try_push_str(value)?;
+        Ok(buf)
     }
 
-    #[inline(always)]
-    pub fn as_str_mut(&mut self) -> &mut str {
-        unsafe {
-            std::str::from_utf8_unchecked_mut(std::slice::from_raw_parts_mut(
-                self.as_ptr(),
-                self.len(),
-            ))
+    pub fn try_from_f64(value: f64, cap: u32) -> Result<Self> {
+        let mut num = ryu::Buffer::new();
+        let value = num.format(value);
+
+        if value.len() > cap as usize {
+            anyhow::bail!("Text buffer is too small for this f64");
         }
+
+        let mut buf = Self::new(cap);
+        buf.try_push_str(value)?;
+        Ok(buf)
     }
 
     #[inline(always)]
-    pub fn as_slice(&self) -> &[u8] {
-        unsafe { std::slice::from_raw_parts(self.as_ptr(), self.len()) }
-    }
-
-    #[inline(always)]
-    pub fn as_slice_mut(&mut self) -> &mut [u8] {
-        unsafe { std::slice::from_raw_parts_mut(self.as_ptr(), self.len()) }
-    }
-
-    #[inline(always)]
-    pub fn len(&self) -> usize {
+    pub fn len(&self) -> u32 {
         self.0.len()
     }
 
     #[inline(always)]
-    pub fn capacity(&self) -> usize {
+    pub fn capacity(&self) -> u32 {
         self.0.capacity()
     }
 
     #[allow(dead_code)]
     #[inline(always)]
-    fn available(&self) -> usize {
+    fn available(&self) -> u32 {
         self.capacity() - self.len()
-    }
-
-    #[inline(always)]
-    pub fn is_full(&self) -> bool {
-        self.len() == self.capacity()
     }
 
     #[inline(always)]
@@ -133,16 +77,35 @@ impl Text {
     }
 
     #[inline(always)]
+    pub fn is_full(&self) -> bool {
+        self.len() == self.capacity()
+    }
+
+    #[inline(always)]
     pub fn clear(&mut self) {
         self.0.clear();
     }
 
-    pub fn alloc(&self) -> &Arc<Bump> {
-        self.0.alloc()
+    pub fn try_push_str(&mut self, value: impl AsRef<str>) -> Result<()> {
+        self.0.try_push_bytes(value.as_ref().as_bytes())
     }
 
-    pub fn try_push_str(&mut self, value: &str) -> Result<()> {
-        self.0.try_push_bytes(value.as_bytes())
+    pub fn into_bytes(self) -> Bytes {
+        self.0
+    }
+
+    pub fn as_str(&self) -> &str {
+        // SAFETY: Text is guaranteed to be valid UTF-8
+        unsafe { std::str::from_utf8_unchecked(self.0.as_slice()) }
+    }
+
+    pub fn as_str_mut(&mut self) -> &mut str {
+        // SAFETY: Text is guaranteed to be valid UTF-8
+        unsafe { std::str::from_utf8_unchecked_mut(self.0.as_slice_mut()) }
+    }
+
+    pub fn as_bytes(&self) -> &[u8] {
+        self.0.as_slice()
     }
 }
 
@@ -172,33 +135,21 @@ impl AsMut<str> for Text {
     }
 }
 
-impl AsRef<[u8]> for Text {
-    fn as_ref(&self) -> &[u8] {
-        self.as_slice()
-    }
-}
-
-impl AsMut<[u8]> for Text {
-    fn as_mut(&mut self) -> &mut [u8] {
-        self.as_slice_mut()
-    }
-}
-
 impl std::fmt::Debug for Text {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:?}", self.as_str())
+        self.as_str().fmt(f)
     }
 }
 
 impl std::fmt::Display for Text {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.as_str())
+        self.as_str().fmt(f)
     }
 }
 
 impl serde::Serialize for Text {
     fn serialize<S: serde::Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
-        serializer.serialize_str(self.as_str())
+        self.as_str().serialize(serializer)
     }
 }
 
