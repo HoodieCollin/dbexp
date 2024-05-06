59e0bc1bd9dcc926085985792005e0075fb69c99
Collin Kokotas <collinkokotas@gmail.com>
2024-05-06 10:05:21 -0700

    Rough implementation of hcl based schema definitions

--- hcl_schemas/src/lib.rs
+++ hcl_schemas/src/lib.rs
@@ -1,5 +1,142 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use anyhow::Result;
+use data_types::{bytes::Bytes, text::Text, DataType};
+use hcl::{
+    eval::{Context, Evaluate},
+    Block, Body, Expression,
+};
+
+use primitives::InternalString;
+
+#[derive(Debug, Clone, Copy)]
+pub struct ColumnDef {
+    name: InternalString,
+    data_type: DataType,
+}
+
+impl ColumnDef {
+    pub fn name(&self) -> &InternalString {
+        &self.name
+    }
+
+    pub fn data_type(&self) -> DataType {
+        self.data_type
+    }
+}
+
+const EMAIL_TYPE: DataType = DataType::Text(120);
+const PHONE_TYPE: DataType = DataType::Text(20);
+
+fn parse_data_type(input: &Expression, ctx: &Context) -> Result<DataType> {
+    use Expression::{FuncCall, Variable};
+
+    match input {
+        Variable(name) => match name.as_str() {
+            "Number" => Ok(DataType::Number),
+            "Email" => Ok(EMAIL_TYPE),
+            "Phone" => Ok(PHONE_TYPE),
+            "Timestamp" => Ok(DataType::Timestamp),
+            "Text" => anyhow::bail!("Expected Text to have a length"),
+            _ => anyhow::bail!("Unknown data type: {}", name.as_str()),
+        },
+        FuncCall(f) => {
+            let name = InternalString::new(f.name.as_str())?;
+
+            if f.args.len() != 1 {
+                anyhow::bail!("Expected exactly one argument for type constructor");
+            }
+
+            match name.as_str() {
+                "Text" => {
+                    let max_len = f.args[0].evaluate(ctx)?.as_u64().ok_or_else(|| {
+                        anyhow::anyhow!("Expected positive integer argument for Text")
+                    })?;
+
+                    if max_len > Text::MAX_LEN as u64 {
+                        anyhow::bail!("Text length is too large");
+                    }
+
+                    Ok(DataType::Text(max_len as u32))
+                }
+                "Bytes" => {
+                    let max_len = f.args[0].evaluate(ctx)?.as_u64().ok_or_else(|| {
+                        anyhow::anyhow!("Expected positive integer argument for Bytes")
+                    })?;
+
+                    if max_len > Bytes::MAX_LEN as u64 {
+                        anyhow::bail!("Bytes length is too large");
+                    }
+
+                    Ok(DataType::Bytes(max_len as u32))
+                }
+                _ => anyhow::bail!("Unknown data type: {}", name.as_str()),
+            }
+        }
+        _ => anyhow::bail!("Expected variable or function call for data type"),
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct TableDef {
+    name: InternalString,
+    columns: Vec<ColumnDef>,
+}
+
+impl<'a> TryFrom<(&Block, &Context<'a>)> for TableDef {
+    type Error = anyhow::Error;
+
+    fn try_from(src: (&Block, &Context)) -> Result<Self> {
+        let (block, ctx) = src;
+
+        if block.identifier() != "table" {
+            return Err(anyhow::anyhow!("Expected block identifier 'table'"));
+        }
+
+        let labels = block.labels();
+
+        if labels.len() != 1 {
+            return Err(anyhow::anyhow!("Expected exactly one label"));
+        }
+
+        let name = InternalString::new(labels[0].as_str())?;
+
+        let columns = block
+            .body
+            .attributes()
+            .map(|attr| {
+                let name = InternalString::new(attr.key())?;
+
+                Ok(ColumnDef {
+                    name: InternalString::from(name),
+                    data_type: parse_data_type(attr.expr(), ctx)?,
+                })
+            })
+            .collect::<Result<Vec<_>>>()?;
+
+        Ok(Self { name, columns })
+    }
+}
+
+impl TableDef {
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+
+    pub fn columns(&self) -> &[ColumnDef] {
+        &self.columns
+    }
+}
+
+pub fn parse_hcl(input: &str) -> Result<Vec<TableDef>> {
+    let body: Body = hcl::from_str(input)?;
+    let ctx = Context::default();
+
+    Ok(body
+        .blocks()
+        .filter_map(|block| match TableDef::try_from((block, &ctx)) {
+            Ok(table) => Some(table),
+            _ => None,
+        })
+        .collect::<Vec<_>>())
 }
 
 #[cfg(test)]
@@ -7,8 +144,16 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_parse_hcl() {
+        let input = r#"
+            table "users" {
+                email = Email
+                first = Text(100)
+                last  = Text(100)
+                phone = Phone
+            }
+        "#;
+
+        assert!(parse_hcl(input).is_ok());
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,81 +1,75 @@
+use anyhow::Result;
+use data_types::DataValue;
+use hcl_schemas::parse_hcl;
+use indexmap::IndexMap;
+use mem_table::{object_ids::TableId, DataConfig, Table, TableConfig};
+
 fn main() -> anyhow::Result<()> {
-    // const N: usize = 10;
+    let hcl = r#"
+        table "users" {
+            email = Email
+            first = Text(100)
+            last  = Text(100)
+            phone = Phone
+        }
+    "#;
 
-    // let c1 = DataType::O32;
-    // let mut c1_buf = vec![0; c1.byte_count() * N];
-    // let c1_values = vec![
-    //     DataValue::O32(oid::O32::from_uint(1u32)),
-    //     DataValue::O32(oid::O32::from_uint(2u32)),
-    //     DataValue::O32(oid::O32::from_uint(3u32)),
-    //     DataValue::O32(oid::O32::from_uint(4u32)),
-    //     DataValue::O32(oid::O32::from_uint(5u32)),
-    //     DataValue::O32(oid::O32::from_uint(6u32)),
-    //     DataValue::O32(oid::O32::from_uint(7u32)),
-    //     DataValue::O32(oid::O32::from_uint(8u32)),
-    //     DataValue::O32(oid::O32::from_uint(9u32)),
-    //     DataValue::O32(oid::O32::from_uint(10u32)),
-    // ];
+    let tables = parse_hcl(hcl)?
+        .into_iter()
+        .map(|table_def| {
+            let id = TableId::new();
+            let mut name_mapping = IndexMap::new();
 
-    // let mut c1_unloader = Unloader::new(&mut c1_buf, &c1_values);
+            let columns = table_def
+                .columns()
+                .iter()
+                .enumerate()
+                .map(|(idx, column_def)| {
+                    name_mapping.insert(*column_def.name(), idx);
+                    DataConfig::new(column_def.data_type())
+                })
+                .collect::<Vec<_>>();
 
-    // c1_unloader.all()?;
+            let config = TableConfig::new(&columns)?;
 
-    // let c2 = DataType::Bool;
-    // let mut c2_buf = vec![0; c2.byte_count() * N];
-    // let c2_values = vec![
-    //     DataValue::Bool(true),
-    //     DataValue::Bool(false),
-    //     DataValue::Bool(true),
-    //     DataValue::Bool(false),
-    //     DataValue::Bool(true),
-    //     DataValue::Bool(false),
-    //     DataValue::Bool(true),
-    //     DataValue::Bool(false),
-    //     DataValue::Bool(true),
-    //     DataValue::Bool(false),
-    // ];
+            Table::new(id, config, Some(name_mapping))
+        })
+        .collect::<Result<Vec<_>>>()?;
 
-    // let mut c2_unloader = Unloader::new(&mut c2_buf, &c2_values);
+    println!("{:#?}", tables);
 
-    // c2_unloader.all()?;
+    let users = tables.first().unwrap();
+    let (email_col, first_col, last_col, phone_col) = {
+        let config = users.config();
 
-    // // let c3 = DataType::Text(128);
-    // let c3 = DataType::Integer(IntSize::X16);
-    // let mut c3_buf = vec![0; c3.byte_count() * N];
-    // let c3_values = vec![
-    //     DataValue::try_from_any(c3, 100)?,
-    //     DataValue::try_from_any(c3, 200)?,
-    //     DataValue::try_from_any(c3, 300)?,
-    //     DataValue::try_from_any(c3, 400)?,
-    //     DataValue::try_from_any(c3, 500)?,
-    //     DataValue::try_from_any(c3, 600)?,
-    //     DataValue::try_from_any(c3, 700)?,
-    //     DataValue::try_from_any(c3, 800)?,
-    //     DataValue::try_from_any(c3, 900)?,
-    //     DataValue::try_from_any(c3, 1000)?,
-    // ];
+        (
+            config.columns.get(0).unwrap(),
+            config.columns.get(1).unwrap(),
+            config.columns.get(2).unwrap(),
+            config.columns.get(3).unwrap(),
+        )
+    };
 
-    // let mut c3_unloader = Unloader::new(&mut c3_buf, &c3_values);
+    let res = users.insert(vec![
+        vec![
+            Some(email_col.try_new_value("foobar@example.com")?),
+            Some(first_col.try_new_value("Foo")?),
+            Some(last_col.try_new_value("Bar")?),
+            None,
+        ],
+        vec![
+            Some(email_col.try_new_value("other@example.com")?),
+            Some(first_col.try_new_value("Other")?),
+            None,
+            Some(phone_col.try_new_value("123-456-7890")?),
+        ],
+    ])?;
 
-    // c3_unloader.all()?;
-
-    // let t1 = system.create_table(vec![c1, c2, c3]);
-    // let t_columns = t1.columns();
-    // let mut t_iter = t_columns.iter().copied();
-    // let (c1, c2, c3) = (
-    //     t_iter.next().unwrap(),
-    //     t_iter.next().unwrap(),
-    //     t_iter.next().unwrap(),
-    // );
-
-    // // t1.load_data(vec![(c1, c1_buf), (c2, c2_buf), (c3, c3_buf)])?;
-
-    // // let c1 = DataType::O64;
-    // // let c2 = DataType::Integer(IntSize::X16);
-    // // let c3 = DataType::Integer(IntSize::X16);
-    // // let t2 = system.create_table(vec![c1, c2, c3]);
-
-    // println!("{:#?}", system);
+    println!("{:#?}", res);
+    println!("##############################################################");
+    println!("##############################################################");
+    println!("##############################################################");
+    println!("{:#?}", users);
 
     Ok(())
 }
